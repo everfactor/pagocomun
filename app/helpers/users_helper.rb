9663557d6828191a9253f404fee4f7da0c6167f3
Sergio Márquez <smarquezs@gmail.com>
2026-01-11 00:34:07 +0000

    Fix forms.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -20,7 +20,7 @@ module UsersHelper
     select_options = require_org ? {} : {prompt: t("helpers.select.prompt")}
 
     select_attributes = {
-      class: "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm"
+      class: "block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm"
     }
     select_attributes[:required] = true if require_org
 
